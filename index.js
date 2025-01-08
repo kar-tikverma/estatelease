c381b5869cd7ef5cb0cbabe0cdcec1e9247bafb3
Kartik Verma <verma.kartik067@gmail.com>
2025-01-09 00:18:04 +0530

    Modified session age

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const sessionOptions = {
     saveUninitialized: true,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 20000, //7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
     },
 };
