493faf23cfedcea8403e0a3302525db0c1f75ef8
Kartik <verma.kartik067@gmail.com>
2025-05-27 22:49:20 +0530

    added functionality to check whether a listing is available in all functions

--- controllers/rental.js
+++ controllers/rental.js
@@ -4,8 +4,9 @@ const Rental = require("../models/rental.js");
 module.exports.rentNow = async (req, res) => {
     const listing = await Listing.findById(req.params.id);
 
-    if (listing.isRented) {
-        //
+    if (!listing.isAvailable) {
+        req.flash("error", "Already Booked!");
+        return res.redirect("/");
     }
 
     res.render("rentals/rentForm.ejs", { listing });
@@ -14,8 +15,9 @@ module.exports.rentNow = async (req, res) => {
 module.exports.processDetails = async (req, res) => {
     const listing = await Listing.findById(req.params.id);
 
-    if (listing.isRented) {
-        //
+    if (!listing.isAvailable) {
+        req.flash("error", "Already Booked!");
+        return res.redirect("/");
     }
 
     const details = {
@@ -27,18 +29,35 @@ module.exports.processDetails = async (req, res) => {
 };
 
 module.exports.confirmRental = async (req, res) => {
-    const listing = await Listing.findById(req.params.id);
+    try {
+        const listingId = req.params.id;
 
-    const rentalDetails = {
-        listing: req.params.id,
-        tenant: req.user._id,
-        amountPaid: listing.price,
-        rentalDate: new Date(),
-        status: 1,
-    };
+        const updatedListing = await Listing.findOneAndUpdate(
+            { _id: listingId, isAvailable: true },
+            { isAvailable: false },
+            { new: true }
+        );
 
-    await new Rental(rentalDetails).save();
+        if (!updatedListing) {
+            req.flash("error", "Rental failed. Listing may already be booked.");
+            return res.redirect("/listings");
+        }
 
-    req.flash("success", "Rental Confirmed!");
-    res.redirect("/listings");
+        const rentalDetails = {
+            listing: listingId,
+            tenant: req.user._id,
+            amountPaid: updatedListing.price,
+            rentalDate: new Date(),
+            status: 1,
+        };
+
+        await new Rental(rentalDetails).save();
+
+        req.flash("success", "Rental Confirmed!");
+        res.redirect("/listings");
+    } catch (err) {
+        console.error("Rental error:", err);
+        req.flash("error", "Something went wrong. Try again.");
+        res.redirect("/listings");
+    }
 };
